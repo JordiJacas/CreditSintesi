2d4bfb700098a17ad619de125dac6fb36762567f
alejandrooo96 <alejandrooo96@gmail.com>
2018-05-07 16:30:21 +0200

    Mejorado creacion de tablas

--- routes/tablas_bbdd.js
+++ routes/tablas_bbdd.js
@@ -11,11 +11,27 @@ con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
   /*Create a table named "customers":*/
-  var sql = "CREATE TABLE Ranking (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), tiempo TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)";
+  var sql = "CREATE TABLE ranking (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), tiempo TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)";
   con.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("Table created"); 
-    else;
-    console.log("Table exist");
+    console.log("Table created");
+  });
+
+  var sql = "CREATE TABLE invitacionesPartidas (id INT AUTO_INCREMENT PRIMARY KEY, id_usuario1 INT, id_usuario2 INT)";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
+
+  var sql = "CREATE TABLE partidas (id INT AUTO_INCREMENT PRIMARY KEY, id_personaje1 INT, id_personaje2 INT)";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
+  
+  var sql = "CREATE TABLE users (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), email VARCHAR(255), password VARCHAR(255), token TEXT)";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
   });
 });
