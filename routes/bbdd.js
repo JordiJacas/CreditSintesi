c9c2ef46714c2411be9acb562287907a2960253b
Jordi <jjacasventura@iesesteveterradas.cat>
2018-05-24 16:51:48 +0200

    El primer commit heroku

--- routes/bbdd.js
+++ routes/bbdd.js
@@ -1,11 +1,12 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Contrasenya9"
+  host: "ec2-54-243-235-153.compute-1.amazonaws.com",
+  user: "fekjsstwxzeuxz",
+  password: "68deb33a54d12d8fd255f0581efc1f6cfb2b8523c8118abf4f86f5518935e85f",
+  database: "ddr803gnq42grm",
+  port: 5432
 });
-
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
